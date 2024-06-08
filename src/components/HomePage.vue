41c68fa8d479405a5055ecff5671cf4b0cc59a81
black-dev-x <thiago.dev.x@gmail.com>
2024-06-08 16:14:15 -0300

    simplification

---
+++ src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div><img class="robot" src="../assets/robot-home.png" alt=""></div>
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 500px;
+}
+.get-started {
+  padding-top: 20px;
+  font-size: 32px;
+}
+</style>
